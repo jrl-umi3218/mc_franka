fde60331cb7c0f9acda879353d571cf7f66e4178
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-05 11:49:28 +0100

    feat(non-rt): Emulate sched_yield for non-rt thread
    sched_yield is unsupported for non-rt thread. This causes the main
    rt-thread to run as fast as possible instead of keeping the 1ms
    schedule. This is fixed by pausing the thread for the remaining time
    explicitly.
    
    Side-effect: ShowNetworkWarning seems to always be reporting a single
    missed iteration, this might be due to the fact that the thread does not
    wake up perfectly in time and thus we are indeed one iteration behind.
    To be investigated further.
    
    Tested successfully on the real robot

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,16 @@ int sched_setattr(pid_t pid, const struct sched_attr * attr, unsigned int flags)
 
 int main(int argc, char * argv[])
 {
-  struct sched_attr attr;
+
+uint64_t cycle_ns = 1 * 1000 * 1000; // 1 ms default cycle
+char * MC_RT_FREQ = nullptr;
+if((MC_RT_FREQ = getenv("MC_RT_FREQ")) != nullptr)
+{
+  cycle_ns = atoi(MC_RT_FREQ) * 1000 * 1000;
+}
 
 #ifdef USE_REALTIME
+  struct sched_attr attr;
   /* Lock memory */
   if(mlockall(MCL_CURRENT | MCL_FUTURE) == -1)
   {
@@ -37,40 +44,37 @@ int main(int argc, char * argv[])
     }
     return -2;
   }
-#endif
 
   /* Configure deadline policy */
   memset(&attr, 0, sizeof(attr));
   attr.size = sizeof(attr);
 
-  uint64_t cycle_ns = 1 * 1000 * 1000; // 1 ms default cycle
-  char * MC_RT_FREQ = nullptr;
-  if((MC_RT_FREQ = getenv("MC_RT_FREQ")) != nullptr)
-  {
-    cycle_ns = atoi(MC_RT_FREQ) * 1000 * 1000;
-  }
+#endif
 
   /* Initialize callback (non real-time yet) */
   void * data = mc_franka::init(argc, argv, cycle_ns);
   if(!data)
   {
-    printf("Initialization failed\n");
+    printf("[mc_franka] Initialization failed\n");
     return -2;
   }
 
+
+#ifdef USE_REALTIME
   /* Time reservation */
   attr.sched_policy = SCHED_DEADLINE;
   attr.sched_runtime = attr.sched_deadline = attr.sched_period = cycle_ns; // nanoseconds
 
   printf("Running real-time thread at %fms per cycle\n", cycle_ns / 1e6);
 
-#ifdef USE_REALTIME
   /* Set scheduler policy */
   if(sched_setattr(0, &attr, 0) < 0)
   {
     printf("sched_setattr failed: %m\n");
     return -2;
   }
+#else
+  printf("[mc_franka] Running non real-time thread at %fms per cycle. Cycle time is not guaranteed and the control thread might not wake up consistently. If possible, prefer using a real-time kernel.\n", cycle_ns / 1e6);
 #endif
 
   /* Run */
