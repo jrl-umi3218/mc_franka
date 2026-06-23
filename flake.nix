a042b21c3bb8b30d482ab31883538b24ee6dc281
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-23 16:45:46 +0200

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "mc-franka: interface between mc_rtc and Franka Emika robots (Panda)";
+
+  inputs = {
+    mc-rtc-nix.url = "github:mc-rtc/nixpkgs";
+    flake-parts.follows = "mc-rtc-nix/flake-parts";
+    systems.follows = "mc-rtc-nix/systems";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.mc-rtc-nix.flakeModule
+          {
+            flakoboros = {
+              enableQt = false;
+              rosDistros = [ "jazzy" ];
+              overlays = [
+                (_final: _: {
+                  qt6.qtbase = null;
+                  qt6.wrapQtAppsHook = null;
+                })
+              ];
+
+              overrideAttrs.mc-franka =
+                { pkgs-final, drv-prev, ... }:
+                {
+                  nativeBuildInputs = [ pkgs-final.cmake ];
+                  cmakeFlags = drv-prev.cmakeFlags ++ [ "-DUSE_REALTIME=OFF" ];
+                  src = lib.cleanSource ./.;
+                };
+            };
+
+            # This mc-rtc-superbuild configuration will:
+            # - Define named reusable configurations in `configurations`
+            # - Use explicit runtime (Nix runtime components) vs devel (local/source overlays)
+            # - Generate `${project.name}-<configuration>` and `${project.name}-<configuration>-devel` shells
+            #
+            # This will also generate a .superbuild/mc_rtc.yaml file containg the suitable mc_rtc configuration
+            # Devel dependencies are expected to be installed manually in .superbuild/install
+            #
+            # As always, individual packages can be overridden using flakoboros
+            mc-rtc-superbuild =
+              { pkgs, ... }:
+              {
+                enable = true;
+                # TODO: replace this section with your own configuration presets for your project
+                configurations = {
+                  mc-franka = {
+                    extends = [ "minimal" ];
+                    runtime = {
+                      robots = [
+                        pkgs.mc-panda-lirmm
+                        pkgs.mc-panda
+                      ];
+
+                      apps = [
+                        pkgs.mc-rtc-magnum
+                      ];
+                    };
+                    devel = {
+                      apps = [
+                        pkgs.mc-franka
+                      ];
+                    };
+                  };
+                };
+              };
+          }
+        ];
+      }
+    );
+}
